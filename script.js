b423dd2dcfe4c1bd48d6c0b0b44bdd7570248c49
Miquel Marquès <miquelmb11@gmail.com>
2026-02-06 11:36:14 +0100

    Create LMSGI_JS2 repository with indexhtml, style.css and script.js files with the content of the activity 1 of JS2 subject.

---
+++ script.js
@@ -0,0 +1,65 @@
+
+
+function PasswordShow() {
+    var tipus = document.getElementById("contrasenya");
+    if (tipus.type == "password") {
+        tipus.type = "text";
+    }
+    else {
+        tipus.type = "password";
+    }
+
+};
+function UsuariValid() {
+    
+    var usuarInput = document.getElementById("usuari");
+    var arrova = document.getElementById("arrova");
+    // Condicions regex de trobar un @
+    var arrovalletres = /@/;
+    if (usuarInput.value.match(arrovalletres)) {
+        console.log("correcte");
+        arrova.classList.remove("invalid");
+        arrova.classList.add("valid");
+    } else {
+        arrova.classList.remove("valid");
+        arrova.classList.add("invalid");
+    }
+}
+function ContraValid (){
+    var contraInput = document.getElementById("contrassenya");
+    var Minuscula = document.getElementById("minuscula");
+    var Majuscula = document.getElementById("majuscula");
+    var Numero = document.getElementById("numero");
+    var Longitud = document.getElementById("longitud");
+    // Condicions regex de trobar una minuscula
+    var MinsuculaComprovacio = /[a-z]/g;
+    if (contraInput.value.match(MinsuculaComprovacio)) {
+        console.log("correcte");
+        Minuscula.classList.remove("invalid");
+        Minuscula.classList.add("valid");
+    } else {
+        Minuscula.classList.remove("valid");
+        Minuscula.classList.add("invalid");
+    }
+    // Condicions regex de trobar una majuscula
+    var MajusculaComprovacio = /[A-Z]/g;
+    if (contraInput.value.match(MajusculaComprovacio)) {
+        console.log("correcte");
+        Majuscula.classList.remove("invalid");
+        Majuscula.classList.add("valid");
+    } else {
+        Majuscula.classList.remove("valid");
+        Majuscula.classList.add("invalid");
+    }
+    // Condicions regex de trobar un numero
+     var numeroComprovacio = /[0-9]/g;
+    if (contraInput.value.match(numeroComprovacio)) {
+        console.log("correcte");
+        Numero.classList.remove("invalid");
+        Numero.classList.add("valid");
+    } else {
+        Numero.classList.remove("valid");
+        Numero.classList.add("invalid");
+    }
+
+}
