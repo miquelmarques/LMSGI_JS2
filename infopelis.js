47d423a186828ce12168ebb22330ad72ea52f536
Miquel Marquès <miquelmb11@gmail.com>
2026-02-07 16:04:21 +0100

    Update LMSGI_JS2

---
+++ infopelis.js
@@ -0,0 +1,163 @@
+const films = [
+      
+  {
+    "name": "The Godfather",
+    "year": 1972,
+    "style": "Crime",
+    "director": "Francis Ford Coppola",
+    "actors": ["Marlon Brando", "Al Pacino", "James Caan"],
+    "oscars": 3
+  },
+  {
+    "name": "Pulp Fiction",
+    "year": 1994,
+    "style": "Crime",
+    "director": "Quentin Tarantino",
+    "actors": ["John Travolta", "Uma Thurman", "Samuel L. Jackson"],
+    "oscars": 1
+  },
+  {
+    "name": "Schindler's List",
+    "year": 1993,
+    "style": "Historical Drama",
+    "director": "Steven Spielberg",
+    "actors": ["Liam Neeson", "Ben Kingsley", "Ralph Fiennes"],
+    "oscars": 7
+  },
+  {
+    "name": "The Dark Knight",
+    "year": 2008,
+    "style": "Action",
+    "director": "Christopher Nolan",
+    "actors": ["Christian Bale", "Heath Ledger", "Aaron Eckhart"],
+    "oscars": 2
+  },
+  {
+    "name": "Titanic",
+    "year": 1997,
+    "style": "Romance",
+    "director": "James Cameron",
+    "actors": ["Leonardo DiCaprio", "Kate Winslet", "Billy Zane"],
+    "oscars": 11
+  },
+  {
+    "name": "Forrest Gump",
+    "year": 1994,
+    "style": "Drama",
+    "director": "Robert Zemeckis",
+    "actors": ["Tom Hanks", "Robin Wright", "Gary Sinise"],
+    "oscars": 6
+  },
+  {
+    "name": "The Lord of the Rings: The Return of the King",
+    "year": 2003,
+    "style": "Fantasy",
+    "director": "Peter Jackson",
+    "actors": ["Elijah Wood", "Viggo Mortensen", "Ian McKellen"],
+    "oscars": 11
+  },
+  {
+    "name": "Inception",
+    "year": 2010,
+    "style": "Science Fiction",
+    "director": "Christopher Nolan",
+    "actors": ["Leonardo DiCaprio", "Joseph Gordon-Levitt", "Elliot Page"],
+    "oscars": 4
+  },
+  {
+    "name": "Gladiator",
+    "year": 2000,
+    "style": "Historical Epic",
+    "director": "Ridley Scott",
+    "actors": ["Russell Crowe", "Joaquin Phoenix", "Connie Nielsen"],
+    "oscars": 5
+  },
+  {
+    "name": "The Silence of the Lambs",
+    "year": 1991,
+    "style": "Thriller",
+    "director": "Jonathan Demme",
+    "actors": ["Jodie Foster", "Anthony Hopkins", "Scott Glenn"],
+    "oscars": 5
+  },
+  {
+    "name": "La La Land",
+    "year": 2016,
+    "style": "Musical",
+    "director": "Damien Chazelle",
+    "actors": ["Ryan Gosling", "Emma Stone", "John Legend"],
+    "oscars": 6
+  },
+  {
+    "name": "Parasite",
+    "year": 2019,
+    "style": "Thriller",
+    "director": "Bong Joon-ho",
+    "actors": ["Song Kang-ho", "Lee Sun-kyun", "Cho Yeo-jeong"],
+    "oscars": 4
+  },
+  {
+    "name": "12 Years a Slave",
+    "year": 2013,
+    "style": "Historical Drama",
+    "director": "Steve McQueen",
+    "actors": ["Chiwetel Ejiofor", "Michael Fassbender", "Lupita Nyong'o"],
+    "oscars": 3
+  },
+  {
+    "name": "American Beauty",
+    "year": 1999,
+    "style": "Drama",
+    "director": "Sam Mendes",
+    "actors": ["Kevin Spacey", "Annette Bening", "Thora Birch"],
+    "oscars": 5
+  },
+  {
+    "name": "The Shape of Water",
+    "year": 2017,
+    "style": "Fantasy",
+    "director": "Guillermo del Toro",
+    "actors": ["Sally Hawkins", "Michael Shannon", "Octavia Spencer"],
+    "oscars": 4
+  },
+  {
+    "name": "Slumdog Millionaire",
+    "year": 2008,
+    "style": "Drama",
+    "director": "Danny Boyle",
+    "actors": ["Dev Patel", "Freida Pinto", "Anil Kapoor"],
+    "oscars": 8
+  },
+  {
+    "name": "A Beautiful Mind",
+    "year": 2001,
+    "style": "Biography",
+    "director": "Ron Howard",
+    "actors": ["Russell Crowe", "Jennifer Connelly", "Ed Harris"],
+    "oscars": 4
+  },
+  {
+    "name": "No Country for Old Men",
+    "year": 2007,
+    "style": "Crime",
+    "director": "Ethan and Joel Coen",
+    "actors": ["Tommy Lee Jones", "Javier Bardem", "Josh Brolin"],
+    "oscars": 4
+  },
+  {
+    "name": "Chicago",
+    "year": 2002,
+    "style": "Musical",
+    "director": "Rob Marshall",
+    "actors": ["Renée Zellweger", "Catherine Zeta-Jones", "Richard Gere"],
+    "oscars": 6
+  },
+  {
+    "name": "The Departed",
+    "year": 2006,
+    "style": "Crime Thriller",
+    "director": "Martin Scorsese",
+    "actors": ["Leonardo DiCaprio", "Matt Damon", "Jack Nicholson"],
+    "oscars": 4
+  }
+];
